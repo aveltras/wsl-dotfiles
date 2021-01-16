c29bd8ff1b9d310b6c9f1aa26aee1b463ab57e31
aveltras <romain.viallard@outlook.fr>
2021-01-17 00:46:34 +0100

    Update

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -8,11 +8,16 @@
   programs.home-manager.enable = true;
   programs.bash = {
     enable = true;
+    sessionVariables = {
+      DISPLAY = "$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0";
+    };
     shellAliases = {
       ll = "ls -l";
     };
   };
 
+  fonts.fontconfig.enable = true;
+  
   targets.genericLinux.enable = true;
 
   programs.direnv = {
@@ -33,6 +38,8 @@
   home.stateVersion = "21.03";
 
   home.packages = with pkgs; [
+    emacs
+    jetbrains-mono
     # scaleway-cli
     yadm
     (import (builtins.fetchTarball https://github.com/hercules-ci/arion/tarball/master) {}).arion
---
+++ .emacs.d/init.el
@@ -0,0 +1,22 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+(let ((secret.el (expand-file-name ".secret.el" user-emacs-directory)))
+  (when (file-exists-p secret.el)
+    (load secret.el)))
+
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+
